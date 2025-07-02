4f41961ce10adc0f6fba8ff2e123371e31c20e44
Bruno Rafa <brunorafaelfilho10@gmail.com>
2025-07-02 15:43:54 -0300

    adicionando opções de pesquisa

--- boletos.py
+++ boletos.py
@@ -9,8 +9,11 @@ menu = int(input(""))
 
 if menu == 1:
 	os.system("cls")
-	print("teste")
+	numero_boleto = int(input("Digite o número do Boleto: "))
+	data_boleto = input("Digite a data do vencimento do boleto (XX/XX/XXX) :")
 elif menu == 2:
 	os.system("cls")
+	busca = int(input("Digite o número do Boleto para consulta: "))
 elif menu == 3:
 	os.system("cls")
+	busca = int(input("Digite o número do Boleto para exclusão: "))
