71030db2f4496c201a362766a66909aedd4ef24e
Bruno Rafa <brunorafaelfilho10@gmail.com>
2025-07-02 12:00:30 -0300

    Adicionando arquivo com estrutura inicial

---
+++ boletos.py
@@ -0,0 +1,16 @@
+# Algoritmo para eu guardar boletos que são parcelados
+import os
+
+print("O que deseja fazer: ")
+print("1 - Adicionar Boleto")
+print("2 - Consultar Boletos")
+print("3 - Excluir Boletos")
+menu = int(input(""))
+
+if menu == 1:
+	os.system("cls")
+	print("teste")
+elif menu == 2:
+	os.system("cls")
+elif menu == 3:
+	os.system("cls")
