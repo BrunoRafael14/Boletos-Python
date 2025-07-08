e7ac2b72df717382ba3ca1ea6a50ca200c6fe376
Bruno Rafa <brunorafaelfilho10@gmail.com>
2025-07-07 21:28:30 -0300

    Reformulando código
    Reformulei para ficar um pouco mais legível e consegui consertar algumas coisas com ajuda do DeepSeek

--- boletos.py
+++ boletos.py
@@ -1,20 +1,46 @@
 # Algoritmo para eu guardar boletos que são parcelados
 import os
 
-print("O que deseja fazer: ")
-print("1 - Adicionar Boleto")
-print("2 - Consultar Boletos")
-print("3 - Excluir Boletos")
-menu = int(input(""))
+contagem = "sim"
 
-if menu == 1:
-	os.system("cls")
-	endereco = input("Digite o endereço do Imóvel: ")
-	numero_boleto = int(input("Digite o número do Boleto: "))
-	data_boleto = input("Digite a data do vencimento do boleto (XX/XX/XXX) :")
-elif menu == 2:
-	os.system("cls")
-	busca = int(input("Digite o número do Boleto para consulta: "))
-elif menu == 3:
-	os.system("cls")
-	busca = int(input("Digite o número do Boleto para exclusão: "))
+enderecos = []
+numero_boletos = []
+data_boletos = []
+
+
+while contagem == "sim":
+	print("O que deseja fazer: ")
+	print("1 - Adicionar Boleto")
+	print("2 - Consultar Boletos")
+	print("3 - Excluir Boletos")
+	menu = int(input(""))
+
+	if menu == 1:
+		# os.system("cls")
+		busca_endereco = input("Digite o endereço do Imóvel: ")
+		busca_numero_boleto = input("Digite o número do Boleto: ")
+		busca_data_boleto = input("Digite a data do vencimento do boleto (XX/XX/XXX) :")
+
+		enderecos.append(busca_endereco)
+		numero_boletos.append(busca_numero_boleto)
+		data_boletos.append(busca_data_boleto)
+
+		print(numero_boletos)
+
+	elif menu == 2:
+		# os.system("cls")
+		print(numero_boletos)
+
+		busca = input("Digite o número do Boleto para consulta: ")
+		if busca in numero_boletos:
+			position = numero_boletos.index(busca)
+			print("Endereço é {}, o número do boleto é {} e a data de vencimento do boleto é {}".format(enderecos[position], numero_boletos[position], data_boletos[position]))
+			print (position)
+		else:
+			print("Boleto não cadastrado")
+	elif menu == 3:
+		# os.system("cls")
+		busca = input("Digite o número do Boleto para exclusão: ")
+
+
+	contagem = input("Deseja fazer mais buscas ? ")
