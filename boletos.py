e158ad7bf796f12ab4aa6e16a792bd8edab60612
Bruno Rafa <brunorafaelfilho10@gmail.com>
2025-07-20 23:18:00 -0300

    Update boletos.py

--- boletos.py
+++ boletos.py
@@ -1,11 +1,10 @@
 # Algoritmo para eu guardar boletos que são parcelados
 import os
+import json
 
 contagem = 1
 
-enderecos = []
-numero_boletos = []
-data_boletos = []
+boletos = {}
 
 
 while contagem == 1:
@@ -16,29 +15,59 @@ while contagem == 1:
 	menu = int(input(""))
 
 	if menu == 1:
-		# os.system("cls")
+		os.system("cls")
 		busca_endereco = input("Digite o endereço do Imóvel: ")
 		busca_numero_boleto = input("Digite o número do Boleto: ")
 		busca_data_boleto = input("Digite a data do vencimento do boleto (XX/XX/XXX) :")
 
-		enderecos.append(busca_endereco)
-		numero_boletos.append(busca_numero_boleto)
-		data_boletos.append(busca_data_boleto)
+		boletos[busca_numero_boleto] = {
+			"endereço" : busca_endereco,
+			"vencimento do boleto" : busca_data_boleto
+		}
 
-		print("Boleto número {} cadastrado com sucesso!".format(numero_boletos))
+		caminho_json = "dados/boletos.json"
+
+		with open(caminho_json, 'w', encoding='utf-8') as arquivo:
+			json.dump(boletos, arquivo, ensure_ascii=False, indent=4)
+
+		print("Boleto número {} cadastrado com sucesso!".format(busca_numero_boleto))
 
 	elif menu == 2:
-		# os.system("cls")
+		os.system("cls")
+		with open("dados/boletos.json", 'r', encoding='utf-8') as arquivo:
+			boletos = json.load(arquivo)
 
-		busca = input("Digite o número do Boleto para consulta: ")
-		if busca in numero_boletos:
-			position = numero_boletos.index(busca)
-			print("Endereço é {}, o número do boleto é {} e a data de vencimento do boleto é {}".format(enderecos[position], numero_boletos[position], data_boletos[position]))
+		tipo_pesquisa = int(input("Gostaria de Pesquisar um boleto em específico ou vizualizar todos os boletos ? \n1 - BuscaIndividual \n2 - Busca Geral \n"))
+		if tipo_pesquisa == 1:
+			busca = input("Digite o número do Boleto para consulta: ")
+			if busca in boletos:
+				position = boletos[busca]
+				print(position) 
+			else:
+				print("Boleto não cadastrado")
+		elif tipo_pesquisa == 2:
+			os.system("cls")
+			for boleto in boletos:
+				dados = boletos[boleto]
+				print("Boleto nº: {} \n Endereço: {}\n Vencimento: {} \n--------------------".format(boleto, dados["endereço"], dados["vencimento do boleto"]))
 		else:
-			print("Boleto não cadastrado")
-	elif menu == 3:
-		# os.system("cls")
-		busca = input("Digite o número do Boleto para exclusão: ")
+			print("Opção inválida")
 
+	elif menu == 3:
+		os.system("cls")
+		#Abrir o arquivo e salvar na variável boletos
+		with open("dados/boletos.json", 'r', encoding='utf-8') as arquivo:
+			boletos = json.load(arquivo)
+		
+		#Buscar o boletos e excluir com a função pop()
+		busca = input("Digite o número do Boleto para exclusão: ")
+		if busca in boletos:
+			exclusao = boletos.pop(busca)
+
+		#Abaixo é o comando para salvar a exclusão no arquivo json
+		caminho_json = "dados/boletos.json"
+		with open(caminho_json, 'w', encoding='utf-8') as arquivo:
+			json.dump(boletos, arquivo, ensure_ascii=False, indent=4)
 
 	contagem = int(input("Deseja fazer mais buscas ? \n1 - Sim \n2 - Não \n "))
+	os.system("cls")
